5b6fcfc1fcdb805a10bc5200d7adc131109a2f79
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 14:51:01 -0300

    Create questaoTres.java

---
+++ questaoTres.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Vamos calcular a área e o perimetro de uma circunferência.");
+
+    System.out.println("Digite o valor do raio da circunferência: ");
+    float raio = scanner.nextFloat();
+
+    float area = (raio * raio) * 3.14f;
+    float perimetro = 2 * 3.14f * raio;
+    System.out.println("A área da circunferência é "+area + " e o perimetro é "+perimetro);
+    
+  }
+}
