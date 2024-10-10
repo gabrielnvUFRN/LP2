51375c0eb46cb6324ca752cc55a3897a0dc09a89
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 15:00:53 -0300

    Create questaoCinco.java

---
+++ questaoCinco.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class questaoCinco{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite dois números inteiros para calcularmos o resultado da divisão e o resto: ");
+    int num1 = scanner.nextInt();
+    int num2 = scanner.nextInt();
+
+    int divisao = num1 / num2;
+    int resto = num1 % num2;
+
+    System.out.println("O resultado da divisão é "+divisao +" e o resto é "+resto);
+    
+  }
+}
