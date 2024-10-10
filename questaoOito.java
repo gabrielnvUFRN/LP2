e5ed39a7b94076b41e77af249af6d54ea65f3ff7
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 15:16:44 -0300

    Create questaoOito.java

---
+++ questaoOito.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class questaoOito{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Vamos calcular o volume de uma lata de oléo.");
+
+    System.out.println("Digite o raio: ");
+    float raio = scanner.nextFloat();
+    System.out.println("Digite a altura: ");
+    float altura = scanner.nextFloat();
+
+    float volume = 3.14f * raio * raio * altura;
+    System.out.println("O volume da lata é: "+volume);
+    
+  }
+}
