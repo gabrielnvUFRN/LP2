981d4ddb737d4f8e117ce9806ae68bcf88406d78
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 14:38:14 -0300

    Create questaoUm.java

---
+++ questaoUm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class questaoUm{
+  public static void main(String[] args){
+  Scanner scanner = new Scanner(System.in);
+    System.out.println("Vamos calcular a área e o perimetro de um retângulo.");
+    
+    System.out.println("Digite o tamanho da base do retângulo: ");
+    float base = scanner.nextFloat();
+    
+    System.out.println("Digite o tamanho da altura do retângulo: ");
+    float altura = scanner.nextFloat();
+    
+    float area = base * altura;
+    float perimetro = 2 * (base + altura);
+    System.out.println("A área do retângulo é: "+area);
+    System.out.println("O perimetro do retângulo é: "+perimetro);
+    
+    
+    
+  }
+}
