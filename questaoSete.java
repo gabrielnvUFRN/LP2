a055db172933485b27d9164494511fe083e75848
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 15:12:53 -0300

    Create questaoSete.java

---
+++ questaoSete.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class questaoSete{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a temperatura em Fahrenheit para que ela seja exibida na escala Celsius: ");
+    float fahrenheit = scanner.nextFloat();
+
+    float celsius = (fahrenheit - 32) * 5/9;
+    System.out.println("A temperatura em Celsius é: "+celsius);
+    
+  }
+}
