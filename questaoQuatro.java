16fe9ea0c25926093b001b801cd2a66122c08c86
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 14:55:21 -0300

    Create questaoQuatro.java

---
+++ questaoQuatro.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class questaoQuatro{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Vamos calcular o perimetro de um triângulo.");
+
+    System.out.println("Digite o lado 1: ");
+    float lado1 = scanner.nextFloat();
+    System.out.println("Digite o lado 2: ");
+    float lado2 = scanner.nextFloat();
+    System.out.println("Digite o lado 3: ");
+    float lado3 = scanner.nextFloat();
+
+    float perimetro = lado1 + lado2 + lado3;
+    System.out.println("O perimetro do triângulo é: "+perimetro);
+    
+    
+    
+    
+  }
+}
