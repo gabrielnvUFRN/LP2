edeb5b7b1f07b5ba888d601c1e4ac38099de2841
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-10 14:45:00 -0300

    Create questaoDois.java

---
+++ questaoDois.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Vamos calcular a área e o perimetro de um quadrado.");
+
+    System.out.println("Digite o tamanho do lado do quadrado: ");
+    float lado = scanner.nextFloat();
+
+    float area = lado * lado;
+    float perimetro = lado * 4;
+
+    System.out.println("O valor da área é: "+area +" e o valor do perimetro é: "+perimetro);
+  }
+}
